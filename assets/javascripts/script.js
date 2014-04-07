078c0a29a6144b2e28fbf19f5e837ff2090bd065
Arshad Chummun <arshad@chummun.com>
2014-04-07 18:29:36 +0400

    Some js for menu hover dropdowns and equal col height

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -3,5 +3,21 @@
  * Custom scripts for theme.
  */
 (function ($) {
-  // code here
+  $(document).ready(function() {
+    // Show dropdowns on hover.
+    $('.nav > li.dropdown').mouseenter(function() {
+      $(this).addClass('open');
+    });
+    $('.nav > li.dropdown').mouseleave(function() {
+      $(this).removeClass('open');
+    });
+
+    // Equal height for .row-eh and .col-eh
+    $('.row-eh, .col-eh').eh();
+  });
+
+  // Sets all el to equal height.
+  $.fn.eh = function() {
+    return this.height(Math.max.apply(this, $.map(this, function(el) { return $(el).outerHeight() })));
+  }
 })(jQuery);
